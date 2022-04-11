68549ae00370491da9f27ac5cfb654d46b6a1888
Nikita Zhyburtovich <n.zhyburtovich@gmail.com>
2022-04-11 19:35:22 +0200

    try base url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "https://auth.nomoreparties.co";
+export const BASE_URL = 'http://api.nzhyburtovich.nomoredomains.work';
 
 const getJson = (response) => {
   if (response.ok) {
